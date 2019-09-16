1abce5f04a2afe13d64b0692a4bf36d9fac7ae9d
eduardo <edumitjans@gmail.com>
2019-09-16 20:43:39 -0300

    Endpoint devuelve JSON exitosamente

--- endpoint.js
+++ endpoint.js
@@ -1,7 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 80
+const express = require('express');
+const app = express();
+const port = 80;
+const fs = require('fs');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+function getdatos(aafe,result){
+  return fs.readFileSync('datos.json','utf-8');
+}
+
+app.get('/datos', (req, res) => {
+  res.type("json");
+  var response = getdatos(req.query.aafe);
+  res.end(response);
+});
 
 app.listen(port, () => console.log(`Escuchando en puerto ${port}!`))
